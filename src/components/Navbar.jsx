eea03036d24577c1429bf916c7964bf547beb8da
Eyohen <henry.eyo2@gmail.com>
2024-01-31 17:24:34 +0100

    some new things

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,65 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import logo from '../assets/logo.png'
 import { RiArrowDownSLine } from "react-icons/ri";
 import { RiMenuFill } from "react-icons/ri";
+import { SlMagnifier } from "react-icons/sl";
+// import { Link, useParams, useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+    const [open, setOpen] = useState()
+    const [data, setData] = useState([])
+    // const navigate = useNavigate()
+
+    const handleOpen = () => {
+        setOpen((prev) => !prev)
+    }
+
   return (
     <div className=''>
         
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between px-64'>
             
  
         <img src={logo} alt='' className='w-[150px] h-12 object-cover mx-6'/>
  
 
-<div className='flex gap-6 mr-64'>
-        <p className='text-[#201327] text-xl font-light hidden md:block'>pricing</p>
+{/* <div className='flex gap-6 mr-64'>
+        <p className='text-[#201327] text-xl font-light hidden md:block'>Pricing</p>
         <p className='text-[#201327] text-xl font-light hidden md:block'>Explore Communities</p>
         <p className='text-[#201327] text-xl font-light hidden md:block'>Community Owners</p>
-        </div>
+        </div> */}
         
         <RiMenuFill size={30} className='md:hidden' />
-<div className='flex gap-1 items-center px-6 '>
-        <p className='text-[#201327] text-xl font-bold hidden md:block'>login </p>
-        <RiArrowDownSLine size={25} className='pt-1 hidden md:block'/>
+
+
+<div className='flex gap-9 items-center px-6 '>
+<p className='text-[#201327] text-xl font-light hidden md:block'>Pricing</p>
+        <p className='text-[#201327] text-xl font-light hidden md:block'>Explore Communities</p>
+        <p className='text-[#201327] text-xl font-light hidden md:block'>Community Owners</p>
+        <p className='text-white px-4 rounded-md text-xl font-semibold hidden md:block bg-[#F08E1F]'>login </p>
+        {/* <RiArrowDownSLine size={25} className='pt-1 hidden md:block'/> */}
+        <SlMagnifier size={25} onClick={handleOpen}/>
         </div>
 
-
+      
 
         </div>
+        {open ?  (<div class="block absolute w-full bg-white right-[1px] p-4 shadow-md z-30 text-center">
+            <div>
+                <div className='flex px-6'>
+                <input placeholder='search communities...' className='w-full px-4' />
+                <button className='bg-[#F08E1F] text-white px-2 rounded-md font-semibold '>Search</button>
+                </div>
+                {/* <p className='font-bold w-full'>{data.firstName}</p>
+                <Link to={'/dashboard'}><p className='hover:bg-gray-300 rounded-md px-12 py-1 text-lg'>Dashboard</p></Link>
+        <Link to={'/employee'}><p className='hover:bg-gray-300 rounded-md px-12 py-1 text-lg'>Employees</p></Link>
+        <Link to={'/receipt'}><p className='hover:bg-gray-300 rounded-md px-12 py-1 text-lg'>Receipts</p></Link>
+
+        <Link to={'/task'}><p className='hover:bg-gray-300 rounded-md px-12 py-1 text-lg'>Tasks</p></Link> */}
+               
+            </div> 
+  </div>) : (null)}
 
         {/* <p className='text-[#F08E1F] text-xl font-bold'>login </p>
         <p className='text-[#F3D8A7] text-xl font-bold'>login </p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,99 @@ import { RiArrowRightSLine } from "react-icons/ri";
 import slack from '../assets/slackimage.jpg'
 import telegram from '../assets/telegram.jpeg'
 import whatsApp from '../assets/whatsapp.png'
-import spaces from '../assets/googlespaces.jpg'
+import spaces from '../assets/spacesremove.png'
 import linkedin from '../assets/linkedin.png'
 import discord from '../assets/discord.png'
 import reddit from '../assets/reddit.png'
-import { RiAddCircleFill } from "react-icons/ri";
+import { RiAddCircleFill, RiCoupon3Line } from "react-icons/ri";
+import { SlBadge } from "react-icons/sl";
+import pigeonhero from '../assets/Pigeonhire.com.png'
+import image1 from '../assets/Image 1.jpg'
+import image2 from '../assets/Image 2.jpg'
+import image3 from '../assets/Image 3.jpg'
+import image4 from '../assets/Image 4.jpg'
+import image5 from '../assets/Image 5.jpg'
+import image6 from '../assets/Image 6.jpg'
+import image7 from '../assets/Image 7.jpg'
+import image8 from '../assets/Image 8.jpg'
 
+
+const data = [
+    {
+    url:`${image1}`,
+    title:"Green Earth Advocates",
+    activeSince: '2018',
+    badge:"Verified",
+    communityType:"Environmental Activism",
+    memberCount:"1-50",
+    isPaid:"Free"
+    },
+    {
+        url:`${image2}`,
+        title:"Tech Innovators",
+        activeSince: '2016',
+        badge:"Verified",
+        communityType:"Technology and Innovation",
+        memberCount:"2,501 - 5,000",
+        isPaid:"Paid"
+        },
+        {
+            url:`${image3}`,
+            title:"Green Earth Advocates",
+            activeSince: '2018',
+            badge:"Verified",
+            communityType:"Environmental Activism",
+            memberCount:"1-50",
+            isPaid:"Free"
+            },
+            {
+                url:`${image4}`,
+                title:"Green Earth Advocates",
+                activeSince: '2018',
+                badge:"Verified",
+                communityType:"Environmental Activism",
+                memberCount:"1-50",
+                isPaid:"Free"
+                },
+                {
+                    url:`${image5}`,
+                    title:"Green Earth Advocates",
+                    activeSince: '2018',
+                    badge:"Verified",
+                    communityType:"Environmental Activism",
+                    memberCount:"1-50",
+                    isPaid:"Free"
+                    },
+                    {
+                        url:`${image6}`,
+                        title:"Green Earth Advocates",
+                        activeSince: '2018',
+                        badge:"Verified",
+                        communityType:"Environmental Activism",
+                        memberCount:"1-50",
+                        isPaid:"Free"
+                        },
+                        {
+                            url:`${image7}`,
+                            title:"Green Earth Advocates",
+                            activeSince: '2018',
+                            badge:"Verified",
+                            communityType:"Environmental Activism",
+                            memberCount:"1-50",
+                            isPaid:"Free"
+                            },
+                            {
+                                url:`${image8}`,
+                                title:"Green Earth Advocates",
+                                activeSince: '2018',
+                                badge:"Verified",
+                                communityType:"Environmental Activism",
+                                memberCount:"1-50",
+                                isPaid:"Free"
+                                },
+                               
+
+]
 
 
 
@@ -17,38 +104,69 @@ const Home = () => {
   return (
     <div className=''>
     <Navbar/>
-    <div className='bg-[#201327] p-32'>
+    <div className='bg-[#201327] '>
+
+<div className='flex flex-col md:flex-row gap-y-6 items-center justify-center gap-24 '>
+     <div className='mt-32 md:mb-24'>  
     <p className='text-white text-5xl font-bold'>Connect Directly with your</p> 
     <p className='text-white text-5xl font-bold mt-4'>Target Audience, Right </p>
     <p className='text-white text-5xl font-bold mt-4'>Where they Engage.</p>
     <p className='text-white text-xl mt-4'>Expand your outreach effortlessly with our extensive and diverse network.</p> 
     <p className='text-white text-xl mt-2'>Startups and established businesses use our platform to connect with various groups.</p>
-    <button className='bg-[#F08E1F] text-[#201327] rounded-md px-4 py-3 mt-6'><div className='flex items-center gap-2'><p className='text-xl '>Browse Communities</p><p><RiArrowRightSLine size={25}/></p></div></button>
+   <div className='flex flex-col md:flex-row gap-y-4 items-center mt-6'>
+    <button className='bg-[#F08E1F] text-[#201327] rounded-md px-4 py-3 '><div className='flex items-center gap-2'><p className='text-xl '>Browse Communities</p><p><RiArrowRightSLine size={25}/></p></div></button>
     <button className='border-2 border-[#F08E1F] text-[#F08E1F] rounded-md px-4 py-3 ml-4'><div className='flex items-center gap-2'><p className='text-xl '>For Communities Owners</p><p><RiArrowRightSLine size={25}/></p></div></button>
+    </div>
      {/* <p className='text-[#F08E1F] text-xl font-bold'>login </p>
         <p className='text-[#F3D8A7] text-xl font-bold'>login </p>
         <p className='text-[#F3C164] text-xl font-bold'>login </p> */}
-     
+
+  </div> 
+         <img src={pigeonhero} alt='' className='object-cover w-[500px] h-[350px]'/>
+
+
+         </div>
+
+
     </div>
+
+
 
     {/* bragging side with groups */}
 
 
-    <div className='p-32'>
-    <p className='text-[#F08E1F] text-3xl font-bold text-center mt-4'> Discover more than 5,000 valuable groups across 20 countries on these platforms</p>
+    <div className=''>
+    <p className='text-[#F08E1F] text-3xl font-bold text-center mt-16'> Discover more than 5,000 valuable groups across 20 countries on these platforms</p>
 
-<div className='flex gap-9 text-3xl mt-9 justify-center'>
+<div className=''>
+<div className='grid grid-cols-8 gap-[165px] text-3xl mt-16 justify-center items-center ml-[140px] '>
+    <div className='flex gap-4'>
    <img src={slack} className='object-cover w-[75px] h-[75px]'/>
    <p className='text-3xl font-semibold mt-3 text-gray-500'>Slack</p>
+   </div>
+   <div className='flex gap-4'>
    <img src={telegram} className='object-cover w-[75px] h-[75px]' />
    <p className='text-3xl font-semibold mt-3 text-gray-500'>Telegram groups</p>
+   </div>
+   <div className='flex gap-4'>
    <img src={whatsApp} className='object-cover w-[75px] h-[75px]' />
    <p className='text-3xl font-semibold mt-3 text-gray-500'>Whatsapp groups</p>
+   </div>
+
+   {/* <div className='flex gap-4'>
    <img src={spaces} className='object-cover w-64 h-[75px]' />
+   </div> */}
+
+   <div className='flex gap-4'>
    <img src={discord} className='object-cover w-[75px] h-[75px]' />
    <p className='text-3xl font-semibold mt-3 text-gray-500'>Discord</p>
+   </div>
+   <div className='flex gap-4'>
    <img src={reddit} className='object-cover w-[75px] h-[75px]' />
    <p className='text-3xl font-semibold mt-3 text-gray-500'>Reddit</p>
+   </div>
+   </div>
+  
     {/* <p> WhatsApp Community</p>
   
 
@@ -61,7 +179,7 @@ const Home = () => {
 
     {/* how it works */}
 
-<div className='p-16 mt-9'>
+<div className=' mt-24'>
     <p className='text-[#201327] text-center text-2xl font-light'>How It Works</p>
     <p className='text-[#F3C164] text-center text-4xl font-semibold'>Two Simple Ways to Expand Your Reach</p>
 
@@ -115,10 +233,61 @@ const Home = () => {
 
         <p className='ml-12 mt-5 underline text-purple-600 font-semibold'>Get Started Today</p>
     </div>
-
-
     </div>
 
+
+{/* featured communities */}
+<div className=' mt-24 mb-16'>
+    <div className='flex justify-between items-center px-32 '>
+<p className='font-bold text-2xl'>Featured Communities</p>
+<p className='text-[#F3C164] text-lg cursor-pointer'>see all communities</p>
+</div>
+
+<div className='grid grid-cols md:grid-cols-4 mt-4 gap-4 px-32'>
+    {data.map((item, index) => (
+    
+<div className='border border-white p-1'>
+    {/* <img src='https://img.freepik.com/premium-photo/full-shot-girl-learning-math-school_23-2150470852.jpg?t=st=1706626764~exp=1706627364~hmac=e7442b0d6c6764a1ab4a09060fb80280c346e2e076a194a282ec3905330e99d2&w=826' alt='' className='w-[350px] h-[140px] object-cover rounded-md' /> */}
+    <img src={item.url} alt='' className='w-[350px] h-[140px] object-cover rounded-md' />
+    <p className='font-semibold text-xl mt-1'>Green Earth Advocates</p>
+    <p className='text-lg text-gray-400'>Community type : Environmental Activism</p>
+    <div className='flex gap-2 items-center text-gray-400'>
+    <p className=''>Active Since : 2018</p>
+    <p className=''>Member count : 1 - 50</p> 
+    </div>
+
+    <div className='flex gap-2 items-center text-gray-400 mt-2'>
+    <SlBadge /> 
+    <p>Verified</p>
+    <RiCoupon3Line />
+    <p>Free</p>
+    </div> 
+</div> 
+
+    ))}
+
+{/* 
+<div className='border border-white p-1'>
+    <img src='https://img.freepik.com/premium-photo/full-shot-girl-learning-math-school_23-2150470852.jpg?t=st=1706626764~exp=1706627364~hmac=e7442b0d6c6764a1ab4a09060fb80280c346e2e076a194a282ec3905330e99d2&w=826' alt='' className='w-[450px] h-[120px] object-cover rounded-md' />
+    <p className='font-semibold text-xl mt-1'>Green Earth Advocates</p>
+    <p className='text-lg text-gray-400'>Community type : Environmental Activism</p>
+    <div className='flex gap-2 items-center text-gray-400'>
+    <p className=''>Active Since : 2018</p>
+    <p className=''>Member count : 1 - 50</p> 
+    </div>
+
+    <div className='flex gap-2 items-center text-gray-400 mt-2'>
+    <SlBadge /> 
+    <p>Verified</p>
+    <RiCoupon3Line />
+    <p>Free</p>
+    </div> 
+</div>  */}
+
+
+</div>
+
+</div>
 
 </div>
     </div>
