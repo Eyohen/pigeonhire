0acf9bf9642cdbb451a4cca23c36a5134b1debaa
valentina buoro <valentinabuoro4@gmail.com>
2024-05-23 19:52:15 +0100

    why pigeonhire

--- src/components/OurDiverseUser.jsx
+++ src/components/OurDiverseUser.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const OurDiverseUser = () => {
   return (
-    <div className='py-9 bg-white px-4 md:px-[120px]'>
-        <p className='text-center text-4xl font-bold'>Our Diverse User Community</p>
+    <div className='py-9 bg-white px-4 md:px-[120px] font-nunito'>
+        <p className='text-center text-[#0B0B0E] text-4xl font-bold'>Our Diverse User Community</p>
 
-        <p className='text-700 text-xl'>Our platform connects you to diverse opportunities for meaningful collaborations and partnerships, helping you engage with your target audiences and amplify their impact worldwide.</p>
+        <p className='font-normal text-center text-3xl'>Our platform connects you to diverse opportunities for meaningful collaborations and partnerships, helping you engage with your target audiences and amplify their impact worldwide.</p>
 
 
     </div>
