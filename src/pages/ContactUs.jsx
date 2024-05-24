9f747cf8b5a2220df114b9d5f0ab05e78af9138e
valentina buoro <valentinabuoro4@gmail.com>
2024-05-24 16:22:09 +0100

    wip

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -9,24 +9,24 @@ const ContactUs = () => {
     <div className="bg-gray-100 font-nunito p-9">
    
 
-<div className='border border-gray-600 rounded-2xl px-32 py-12 flex flex-col'>
+<div className='border border-gray-600 rounded-2xl px-48 pt-24 pb-12 flex flex-col'>
 
 <div>
-     <p className='text-gray-600'>Name</p>
-      <input className='w-full px-2 py-2 bg-white border border-gray-200 rounded-lg' />
+     <p className='text-[#2A2B2B] text-3xl font-normal'>Name</p>
+      <input className='w-full px-2 py-4 bg-white border border-gray-200 rounded-lg mb-4' />
 
-      <p className='text-gray-600 mt-4'>Email Address</p>
-      <input className='w-full px-2 py-2 bg-white border border-gray-200 rounded-lg' />
+      <p className='text-[#2A2B2B] mt-4 text-3xl font-normal'>Email Address</p>
+      <input className='w-full px-2 py-4 bg-white border border-gray-200 rounded-lg mb-4' />
 
 
-      <p className='text-gray-600 mt-4'>Select Reason</p>
-      <input className='w-full px-2 py-2 bg-white border border-gray-200 rounded-lg' />
+      <p className='text-[#2A2B2B] mt-4 text-3xl font-normal'>Select Reason</p>
+      <input className='w-full px-2 py-4 bg-white border border-gray-200 rounded-lg mb-4' />
 
-      <p className='text-gray-600 mt-4'>Subject</p>
-      <input className='w-full px-2 py-2 bg-white border border-gray-200 rounded-lg' />
+      <p className='text-[#2A2B2B] mt-4 text-3xl font-normal'>Subject</p>
+      <input className='w-full px-2 py-4 bg-white border border-gray-200 rounded-lg mb-4' />
 
-      <p className='text-gray-600 mt-4'>Message</p>
-      <textarea className='w-full px-2 py-2 bg-white border border-gray-200 rounded-lg' />
+      <p className='text-[#2A2B2B] mt-4 text-3xl font-normal'>Message</p>
+      <textarea className='w-full px-2 py-4 bg-white border border-gray-200 rounded-lg mb-4' />
 
     <button className='text-white bg-[#F08E1F] py-2 w-full rounded-full mt-4' >Submit</button>
 
