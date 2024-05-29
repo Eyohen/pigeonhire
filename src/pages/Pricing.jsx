5ae7e4a9d0a5d7973b4c72663b4b2af30ee8a2e6
Eyohen <henry.eyo2@gmail.com>
2024-05-29 10:49:43 +0100

    pricing page touch

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -14,14 +14,14 @@ const Pricing = () => {
 
       <div className="bg-[#FAD5A5] py-12 mt-16 ">
         <p className="text-6xl font-semibold text-center">
-          Pigeonhire Pricing and Plans
+          Pricing and Plans
         </p>
       </div>
 
       <div className="grid grid-cols-2 mt-12 gap-x-24 gap-y-12 px-[120px]">
         <div>
-          <div className="bg-[#201327] py-4 px-24"></div>
-          <div className="shadow-xl px-[120px] py-6">
+          <div className="bg-[#201327] py-2 px-24"></div>
+          <div className="shadow-xl px-[120px] py-2">
             <p className="text-2xl font-semibold text-center">
               To Explore Communities
             </p>
@@ -30,8 +30,8 @@ const Pricing = () => {
         </div>
 
         <div>
-          <div className="bg-[#201327] py-4 px-24"></div>
-          <div className="shadow-xl bg-[#F3D8A7] px-[120px] py-6">
+          <div className="bg-[#201327] py-2 px-24"></div>
+          <div className="shadow-xl bg-[#F3D8A7] px-[120px] py-2">
             <p className="text-2xl font-semibold text-center">
               Find Community Owners
             </p>
@@ -42,11 +42,11 @@ const Pricing = () => {
         <div>
           <div className="bg-[#FFE5B4] py-6 px-24">
             <p className="text-2xl font-medium text-center">Essentials</p>
-            <p className="text-lg text-center">Subscribe</p>
+            <p className="text-lg text-center">Monthly</p>
           </div>
           <div className="shadow-xl px-[20px] py-6">
             <p className="text-2xl font-semibold text-center">
-              Starts at $19.95/month
+              $4.99
             </p>
             <div className="flex items-center gap-x-3">
               <img src={check} className="w-6 h-6" />
@@ -81,7 +81,7 @@ const Pricing = () => {
             </div>
             <div className="flex items-center gap-x-3">
               <img src={close} className="w-6 h-6" />
-              <p className="text-lg mt-4">Save 50% per month</p>
+              <p className="text-lg mt-4">Save 0% per month</p>
             </div>
             <div className="flex justify-center mt-8">
               <button className="bg-[#F08E1F] w-[400px] text-white rounded-full px-6 py-4">
@@ -94,11 +94,11 @@ const Pricing = () => {
         <div>
           <div className="bg-[#A9A9A9] py-6 px-24">
             <p className="text-2xl font-medium text-center">Pro</p>
-            <p className="text-lg text-center">Subscribe</p>
+            <p className="text-lg text-center">6 Months Subscription</p>
           </div>
           <div className="shadow-xl px-[20px] py-6">
             <p className="text-2xl font-semibold text-center">
-              Starts at $62.96/ 6 months
+              $23.95
             </p>
             <div className="flex items-center gap-x-3">
               <img src={check} className="w-6 h-6" />
@@ -132,8 +132,8 @@ const Pricing = () => {
               <p className="text-lg mt-4">Advanced search</p>
             </div>
             <div className="flex items-center gap-x-3">
-              <img src={close} className="w-6 h-6" />
-              <p className="text-lg mt-4">Save 50% per month</p>
+              <img src={check} className="w-6 h-6" />
+              <p className="text-lg mt-4">Save 15% per month</p>
             </div>
             <div className="flex justify-center mt-8">
               <button className="bg-[#F08E1F] w-[400px] text-white rounded-full px-6 py-4">
@@ -146,11 +146,11 @@ const Pricing = () => {
         <div>
           <div className="bg-[#FFE5B4] py-6 px-24">
             <p className="text-2xl font-medium text-center">Premier</p>
-            <p className="text-lg text-center">Subscribe</p>
+            <p className="text-lg text-center">1 year Subscription</p>
           </div>
           <div className="shadow-xl px-[20px] py-6">
             <p className="text-2xl font-semibold text-center">
-              Starts at $89.94/Annum
+              $41.92
             </p>
             <div className="flex items-center gap-x-3">
               <img src={check} className="w-6 h-6" />
@@ -185,7 +185,7 @@ const Pricing = () => {
             </div>
             <div className="flex items-center gap-x-3">
               <img src={check} className="w-6 h-6" />
-              <p className="text-lg mt-4">Save 50% per month</p>
+              <p className="text-lg mt-4">Save 30% per month</p>
             </div>
 
             <div className="flex justify-center mt-8">
